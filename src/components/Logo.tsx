adf3f29405d10e61f2b02db3a266a11c0066c5d9
iVeluu <velualex10@gmail.com>
2025-08-24 12:25:32 -0600

    add ceti logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,10 @@
-
 export default function Logo() {
   return (
-    <div>
-      <img className="h-32 sm:mx-auto sm:mb-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/CETI_Logo.png/330px-CETI_Logo.png" alt="Logo Ceti" />
+    <div className="sm:flex sm:items-center sm:justify-center h-full">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/CETI_Logo.png/330px-CETI_Logo.png"
+        alt="Logo Ceti"
+      />
     </div>
   )
 }
